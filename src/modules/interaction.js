309c11ecdd4ebcc3b44cdeb4d8514c970bf206a7
Botlhale Setou <botlhale.setou@gmail.com>
2022-10-15 00:32:21 +0200

    Correct linter errors #2

--- src/modules/interaction.js
+++ src/modules/interaction.js
@@ -1,6 +1,5 @@
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jX8uSQVUyjJLw1sq1HTq/scores';
 
-
 export const fetchData = async () => {
     const response = await fetch(url);
     const {result} = await response.json();
--- src/modules/render.js
+++ src/modules/render.js
@@ -9,20 +9,4 @@ export const render = (scores) => {
     scores.sort((a, b) => b.score - a.score).forEach((score) => {
         outputDiv.innerHTML += `<p>${score.user}: ${score.score}</p>`;
     });
-
-    // console.log(scores);
-    // for (let entry in scores) {
-    //   const entryDiv = document.createElement('div');
-    //   const entryText = document.createElement('p');
-
-    //   console.log('here: ' + entry['user']);
-
-    //   entryText.textContent = `${entry.user}: ${entry.score}`;
-
-    //   entryText.classList.add('entry-text');
-    //   entryDiv.classList.add('entry-div');
-
-    //   entryDiv.append(entryText);
-    //   outputDiv.append(entryDiv);
-    // };
 };
